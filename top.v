2cc24b31c602b3440af6c4b3f48420d543e290c9
HCT <207287409@qq.com>
2026-01-26 20:46:05 +0800

    合并

--- top.v
+++ top.v
@@ -1,41 +1,36 @@
 `timescale 1ns / 1ps
-// mycpu_top - SoC????
+// mycpu_top - SoC
 module top(
     input wire clk,
-    input wire resetn,           // ?????
-    input wire [5:0] ext_int,    // ????
+    input wire resetn,
+    input wire [5:0] ext_int,
     
-    // ??SRAM??
-    output wire        inst_sram_en,
-    output wire [3:0]  inst_sram_wen,
-    output wire [31:0] inst_sram_addr,
-    output wire [31:0] inst_sram_wdata,
-    input  wire [31:0] inst_sram_rdata,
-    
-    // ??SRAM??
-    output wire        data_sram_en,
-    output wire [3:0]  data_sram_wen,
-    output wire [31:0] data_sram_addr,
-    output wire [31:0] data_sram_wdata,
-    input  wire [31:0] data_sram_rdata,
-    
-    // ????
+    // 指令SRAM接口
+    output wire inst_sram_en,
+    output wire [3:0] inst_sram_wen,
+    output wire [31:0] inst_sram_addr, inst_sram_wdata,
+    input wire [31:0] inst_sram_rdata,
+    // 数据SRAM接口
+    output wire data_sram_en,
+    output wire [3:0] data_sram_wen,
+    output wire [31:0] data_sram_addr, data_sram_wdata,
+    input wire [31:0] data_sram_rdata,
+    // 调试信号
     output wire [31:0] debug_wb_pc,
-    output wire [3:0]  debug_wb_rf_wen,
-    output wire [4:0]  debug_wb_rf_wnum,
+    output wire [3:0] debug_wb_rf_wen,
+    output wire [4:0] debug_wb_rf_wnum,
     output wire [31:0] debug_wb_rf_wdata
 );
 
-    // ????
     wire [31:0] pc;
     wire [31:0] data_addr;
     
-    // ??SRAM?? - ???????
-    assign inst_sram_en = 1'b1;  // ????????????
+    // SRAM
+    assign inst_sram_en = 1'b1;
     assign inst_sram_wen = 4'b0;
     assign inst_sram_wdata = 32'b0;
     
-    // MMU????
+    // MMU
     wire [31:0] inst_paddr;
     wire [31:0] data_paddr;
     wire no_dcache;
@@ -51,21 +46,21 @@ module top(
     assign inst_sram_addr = inst_paddr;
     assign data_sram_addr = data_paddr;
     
-    // CPU?? - ???????? Block RAM ????????
+    // CPU  Block RAM 
     wire inst_ram_ena_internal;
     mips mips(
-        .clk(~clk),                 // ????
-        .rst(~resetn),              // resetn???????rst??????
+        .clk(~clk),                 // 
+        .rst(~resetn),              // resetn高电平有效
         .instr(inst_sram_rdata),
         .mem_rdata(data_sram_rdata),
-        // ??
+
         .pc(pc),
         .inst_ram_ena(inst_ram_ena_internal),
         .data_ram_ena(data_sram_en),
         .data_ram_wea(data_sram_wen),
         .alu_result(data_addr),
         .mem_wdata(data_sram_wdata),
-        // ????
+        // debug
         .debug_wb_pc(debug_wb_pc),
         .debug_wb_rf_wen(debug_wb_rf_wen),
         .debug_wb_rf_wnum(debug_wb_rf_wnum),
