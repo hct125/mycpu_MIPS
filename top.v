9a50762464ac44a9513d3dd448c3dc64bbdaaeda
444777999 <1340820204@qq.com>
2026-01-26 20:30:30 +0800

    top

--- top.v
+++ top.v
@@ -1,72 +1,75 @@
 `timescale 1ns / 1ps
-//SoC top
+// mycpu_top - SoC????
 module top(
-	input clk,reset,
-    output reg [31:0] pc,
-    output reg [39:0] ascii,
-    output reg [31:0] alu_result,instr,mem_rdata,mem_wdata
+    input wire clk,
+    input wire resetn,           // ?????
+    input wire [5:0] ext_int,    // ????
+    
+    // ??SRAM??
+    output wire        inst_sram_en,
+    output wire [3:0]  inst_sram_wen,
+    output wire [31:0] inst_sram_addr,
+    output wire [31:0] inst_sram_wdata,
+    input  wire [31:0] inst_sram_rdata,
+    
+    // ??SRAM??
+    output wire        data_sram_en,
+    output wire [3:0]  data_sram_wen,
+    output wire [31:0] data_sram_addr,
+    output wire [31:0] data_sram_wdata,
+    input  wire [31:0] data_sram_rdata,
+    
+    // ????
+    output wire [31:0] debug_wb_pc,
+    output wire [3:0]  debug_wb_rf_wen,
+    output wire [4:0]  debug_wb_rf_wnum,
+    output wire [31:0] debug_wb_rf_wdata
+);
+
+    // ????
+    wire [31:0] pc;
+    wire [31:0] data_addr;
+    
+    // ??SRAM?? - ???????
+    assign inst_sram_en = 1'b1;  // ????????????
+    assign inst_sram_wen = 4'b0;
+    assign inst_sram_wdata = 32'b0;
+    
+    // MMU????
+    wire [31:0] inst_paddr;
+    wire [31:0] data_paddr;
+    wire no_dcache;
+    
+    mmu mmu_inst(
+        .inst_vaddr(pc),
+        .inst_paddr(inst_paddr),
+        .data_vaddr(data_addr),
+        .data_paddr(data_paddr),
+        .no_dcache(no_dcache)
     );
-
-    wire        inst_ram_ena;     //如果有inst_en，就用inst_en
-    wire [3:0]  inst_ram_wea = 4'b0;
-    wire [31:0] inst_addr = pc;   //IP核初始化为字节地址
-    wire [31:0] inst_wdata = 32'b0;
-    wire [31:0] inst_rdata;
-    assign instr = inst_rdata;
-
-    wire        data_ram_ena;     //如果有data_en，就用data_en
-    wire [3:0]  data_ram_wea;
-    wire [31:0] data_addr = alu_result;
-    wire [31:0] data_wdata;
-    wire [31:0] data_rdata;
-
-    assign data_wdata = (data_ram_wea == 4'b1111) ? mem_wdata : 
-                        (data_ram_wea == 4'b1100 || data_ram_wea == 4'b0011) ? {2{mem_wdata[15:0]}} : 
-                        {4{mem_wdata[7:0]}};
-    assign mem_rdata = data_rdata;
-
-
-
-    //CPU核
+    
+    assign inst_sram_addr = inst_paddr;
+    assign data_sram_addr = data_paddr;
+    
+    // CPU?? - ???????? Block RAM ????????
+    wire inst_ram_ena_internal;
     mips mips(
-        .clk(clk),
-        .rst(~reset),     //reset高电平复位
-        .instr(instr),              //instrF
-        .mem_rdata(mem_rdata),
-        //outputs
-        .pc(pc),                    //pcF
-        .inst_ram_ena(inst_ram_ena),
-        .data_ram_ena(data_ram_ena),
-        .data_ram_wea(data_ram_wea),
-        .alu_result(alu_result),
-        .mem_wdata(mem_wdata)
+        .clk(~clk),                 // ????
+        .rst(~resetn),              // resetn???????rst??????
+        .instr(inst_sram_rdata),
+        .mem_rdata(data_sram_rdata),
+        // ??
+        .pc(pc),
+        .inst_ram_ena(inst_ram_ena_internal),
+        .data_ram_ena(data_sram_en),
+        .data_ram_wea(data_sram_wen),
+        .alu_result(data_addr),
+        .mem_wdata(data_sram_wdata),
+        // ????
+        .debug_wb_pc(debug_wb_pc),
+        .debug_wb_rf_wen(debug_wb_rf_wen),
+        .debug_wb_rf_wnum(debug_wb_rf_wnum),
+        .debug_wb_rf_wdata(debug_wb_rf_wdata)
     );
 
-    //inst ram
-    inst_ram inst_ram
-    (
-        .clka  (~clk           ),   
-        .ena   (inst_ram_ena   ),
-        .wea   (inst_ram_wea   ),
-        .addra (inst_addr      ),
-        .dina  (inst_wdata     ),
-        .douta (inst_rdata     ) 
-    );
-
-    //data ram
-    data_ram data_ram
-    (
-        .clka  (~clk            ),   
-        .ena   (data_ram_ena    ),
-        .wea   (data_ram_wea    ),
-        .addra (data_addr       ),
-        .dina  (data_wdata      ),
-        .douta (data_rdata      ) 
-    );
-
-    //ascii
-    instdec instdec(
-        .instr(instr),
-        .ascii(ascii)
-    );
 endmodule
